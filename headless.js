845a702f46f320a3d591699600d195a91fb4edfd
Radosław Cymer <cymerrad@gmail.com>
2018-05-23 22:30:29 +0200

    ugly hacks are ugly

--- headless.js
+++ headless.js
@@ -7,12 +7,15 @@ const fs = require('fs');
 
 let argv = require('minimist')(process.argv.slice(2))
 
-try {
+/**
+ * Return settings from argv
+ */
+async function getSettings() {
   let settings = {
     screenshotDir : argv.dir ? argv.dir : 'screenshots',
     dimensions : argv.size ? ((w,h)=>({width:w, height:h}))(...argv.size.split(',')) : {width: 1366, height: 768},
     username : argv.u ? argv.u : (function(){throw "username is required"}()),
-    password : argv.p ? argv.p : (async function(){
+    password : argv.p ? argv.p : await (async function(){
       try {
         let prompt = require('password-prompt'); 
         let password = await prompt('password: '); 
@@ -23,10 +26,8 @@ try {
     }()),
     tributeMessage : argv.m ? argv.m : (function(){throw "message is required"}()),
     recipientID : argv.r ? argv.r : (function(){throw "recipient ID is required"}()),
-  }  
-} catch(e) {
-  console.error('There was an error reading the arguments:', e);
-  return;
+  }
+  return settings;  
 }
 
 /**
@@ -53,6 +54,8 @@ function checkDirectorySync(directory) {
 }
 
 (async() => {
+  let settings = await getSettings();
+
   const browser = await puppeteer.launch({
     // headless: false,
   });
@@ -61,8 +64,8 @@ function checkDirectorySync(directory) {
   await page.goto(`https://www.messenger.com/t/${settings.recipientID}`, {waitUntil: 'networkidle2'});
 
   // Type into email and password box.
-  await page.type('#email', settings.username);
-  await page.type('#pass', settings.password);
+  await page.type('#email', `${settings.username}`);
+  await page.type('#pass', `${settings.password}`);
 
   // Button isn't available at once
   const loginbutton = '#loginbutton';
@@ -72,7 +75,7 @@ function checkDirectorySync(directory) {
   await page.waitForNavigation({waitUntil: 'networkidle2'});  
 
   // Type in message and send with 'enter'
-  await page.type('body', settings.tributeMessage);
+  await page.type('body', `${settings.tributeMessage}`);
   await page.type('body', '\n');
 
   // Giving some time for message to be sent
