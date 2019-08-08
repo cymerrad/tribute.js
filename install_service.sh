6fb41f8d97c76ee104543cab2c5635752d5d1c0d
Radosław Cymer <cymerrad@gmail.com>
2019-08-08 13:42:11 +0200

    Systemd service

--- install_service.sh
+++ install_service.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+
+# this doesn't work on ubuntu, but /etc/systemd/system is a pretty standard location
 sudo cp tribute.js.service $(pkg-config systemd --variable=systemdsystemconfdir)
+
+# location of app.js is variadic -> create .service file from template
+# some things had to be changed
+# missing /var/log dir
 
 sudo systemctl daemon-reload
 sudo systemctl start tribute.js
